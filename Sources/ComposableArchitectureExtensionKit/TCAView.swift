2dff2a5dedf92c83906c4734a32e7652bec4d272
Seyed Mojtaba Hosseini Zeidabadi <Chenzook+GitHub@gmail.com>
2023-03-19 15:30:13 +0330

    feat: add custom `ViewState` ability for `TCAView`s

--- Sources/ComposableArchitectureExtensionKit/TCAView.swift
+++ Sources/ComposableArchitectureExtensionKit/TCAView.swift
@@ -3,7 +3,8 @@ import ComposableArchitecture
 
 public protocol TCAView: View {
     associatedtype Feature: TCAFeature
+    associatedtype ViewState: Equatable
     var store: StoreOf<Feature> { get }
-    var viewStore: ViewStoreOf<Feature> { get }
+    var viewStore: ViewStore<ViewState, Feature.Action> { get }
     init(store: StoreOf<Feature>)
 }
