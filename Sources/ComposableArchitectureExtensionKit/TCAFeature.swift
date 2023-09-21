113e593bb25402bac92e12cb51b8085860d02d11
Seyed Mojtaba Hosseini Zeidabadi <Chenzook+GitHub@gmail.com>
2023-09-21 15:18:27 +0330

    fix: refactor to match the renamed `Effect` type

--- Sources/ComposableArchitectureExtensionKit/TCAFeature.swift
+++ Sources/ComposableArchitectureExtensionKit/TCAFeature.swift
@@ -9,20 +9,20 @@ public protocol TCAFeature: Reducer {
 
 public extension TCAFeature {
     @available(swift, obsoleted: 1.0, message: "Conform to `TCAFeatureViewActionReducer` and implement stubs.")
-    func reduce<T>(into state: inout State, viewAction action: T) -> EffectTask<Action> { fatalError() }
+    func reduce<T>(into state: inout State, viewAction action: T) -> Effect<Action> { fatalError() }
 }
 
 public extension TCAFeature {
     @available(swift, obsoleted: 1.0, message: "Conform to `TCAFeatureDelegateActionReducer` and implement stubs.")
-    func reduce<T>(into state: inout State, delegateAction action: T) -> EffectTask<Action> { fatalError() }
+    func reduce<T>(into state: inout State, delegateAction action: T) -> Effect<Action> { fatalError() }
 }
 
 public extension TCAFeature {
     @available(swift, obsoleted: 1.0, message: "Conform to `TCAFeatureLocalActionReducer` and implement stubs.")
-    func reduce<T>(into state: inout State, localAction action: T) -> EffectTask<Action> { fatalError() }
+    func reduce<T>(into state: inout State, localAction action: T) -> Effect<Action> { fatalError() }
 }
 
 public extension TCAFeature {
     @available(swift, obsoleted: 1.0, message: "Conform to `TCAFeatureChildActionReducer` and implement stubs.")
-    func reduce<T>(into state: inout State, childAction action: T) -> EffectTask<Action> { fatalError() }
+    func reduce<T>(into state: inout State, childAction action: T) -> Effect<Action> { fatalError() }
 }
