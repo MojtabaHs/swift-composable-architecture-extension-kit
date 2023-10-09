5da5a86aaa92ea4138fda6b3e975593cda07d356
Seyed Mojtaba Hosseini Zeidabadi <Chenzook+GitHub@gmail.com>
2023-10-09 18:09:45 +0330

    fix: adapt send view action function to the StoreTask

--- Sources/ComposableArchitectureExtensionKit/TCAViewStore.swift
+++ Sources/ComposableArchitectureExtensionKit/TCAViewStore.swift
@@ -3,5 +3,5 @@ import ComposableArchitecture
 /// More readable function for using view actions inside the body of the View
 public extension ViewStore where ViewAction: TCAFeatureViewAction {
     @discardableResult
-    func send(viewAction action: ViewAction.View) -> ViewStoreTask { send(.view(action)) }
+    func send(viewAction action: ViewAction.View) -> StoreTask { send(.view(action)) }
 }
